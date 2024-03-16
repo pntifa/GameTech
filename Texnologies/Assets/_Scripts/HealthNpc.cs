7dc3ee2f2991916446403bdcfdeb825484ffcdb0
Sotiris <Hadjikiriakousotiris@gmail.com>
2024-03-16 10:03:09 +0200

    Ola extos apo ta artifacts

--- Texnologies/Assets/_Scripts/HealthNpc.cs
+++ Texnologies/Assets/_Scripts/HealthNpc.cs
@@ -5,14 +5,37 @@ using UnityEngine.UI;
 
 public class HealthNpc : MonoBehaviour
 {
-
+    Animator animator;
     public int HP = 100; //defines a certain amount of health to the NPC
 
     public Slider healthbar;
+    [SerializeField] bool player=false;
+
+    void Start(){
+
+    animator = this.GetComponent<Animator>();
+    animator.SetBool("wave",true);
+    
+    }
 
     void Update(){
         healthbar.value=HP; //updates the NPC's healthbar if it takes damage
     }
 
+
+     private void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            player=true;
+            animator.SetBool("wave",false);
+        }
+    }
+    
+     private void OnTriggerExit(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            player=false;
+            animator.SetBool("wave",true);
+        }
+        
+    }
 }
 
---
+++ Texnologies/Assets/_Scripts/healthMonsterScavenger.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthMonsterScavenger : MonoBehaviour
+{
+    public int HP = 100;
+    public int dmg = 20;
+
+    Animator animator;
+
+    [SerializeField] bool player=false;
+    public GameObject monsterObject;
+    public GameObject hpbar;
+    public GameObject coll;
+    bool isOpen = false;
+    [SerializeField] private KeyCode interactKey1 = KeyCode.Alpha1;
+    [SerializeField] private KeyCode interactKey2 = KeyCode.Alpha2;
+
+    public Slider healthbar;
+
+    void Start(){
+
+    animator = this.GetComponent<Animator>();
+    animator.SetBool("attack",true);
+    
+    }
+
+    void Update(){
+        healthbar.value=HP;
+
+        if (player==true)
+            if (Input.GetKeyDown(interactKey1) || Input.GetKeyDown(interactKey2)){
+                isOpen = !isOpen;
+                HP-=dmg;
+            }
+            else if(HP <= 0){
+
+                animator.SetBool("attack",true); //if character is nearby, open the door
+                hpbar.SetActive(false);
+                
+            }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            player=true;
+            animator.SetBool("attack",false);
+        }
+    }
+    
+     private void OnTriggerExit(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            player=false;
+            animator.SetBool("attack",true);
+        }
+        if (HP <= 0){
+            monsterObject.SetActive(false);
+            coll.SetActive(false);
+        }
+    }
+}
